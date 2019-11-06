d9e2e4360930ab2f4659a4a2c2233ba3b70df1c9
github username <github email address>
2019-11-06 15:46:26 +0000

    Done.

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,24 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+#require_relative '../daily_show_guests.csv'
+
+file = File.open('/Users/zoraaver/Development/code/bonus-sql-challenge-london-web-102819/daily_show_guests.csv')
+
+
+open('guest_sql_data.sql', 'w'){ |g|
+
+    file.each do |line|
+        guest_info = line.split(",")
+        year = guest_info[0]
+        occupation = guest_info[1]
+        date = guest_info[2]
+        area = guest_info[3]
+        name = guest_info[4].chomp.delete('"')
+        if name =~ /'/
+            name.insert(name =~ /'/, "'")
+        end
+        g.puts "INSERT INTO guests (year, occupation, date, area, name) VALUES (#{year}, '#{occupation}', '#{date}', '#{area}', '#{name}');"
+    end
+}
+
+
+
--- lib/queries.rb
+++ lib/queries.rb
@@ -1,5 +1,11 @@
 # Write methods that return SQL queries for each part of the challeng here
 
 def guest_with_most_appearances
-  # write your query here!
+  "SELECT name, count(name) FROM guests
+GROUP BY name
+ORDER BY count(name) DESC LIMIT 1;"
 end
+
+"SELECT occupation FROM guests
+GROUP BY year
+ORDER BY count(occupation) DESC;"
